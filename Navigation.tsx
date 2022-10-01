4deda55e62eb9b15f8e8fdecf5f83658c9b5b7c6
David Avalos <davalos@itparral.edu.mx>
2022-10-01 10:27:09 -0600

    (fix) error de instalacion corregido

---
+++ Navigation.tsx
@@ -0,0 +1,28 @@
+import { createDrawerNavigator, DrawerNavigationProp } from '@react-navigation/drawer'
+import { NavigationContainer } from "@react-navigation/native";
+
+import Inicio from './screens/Inicio';
+import Login from './screens/Login';
+
+const Drawer = createDrawerNavigator();
+
+function MyTabs() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{drawerPosition: 'right'}}
+    >
+      <Drawer.Screen name="Home" component={Inicio} 
+        options={{headerShown: false}}
+      />
+      <Drawer.Screen name="Notifications" component={Login} />
+    </Drawer.Navigator>
+  );
+}
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  )
+}
