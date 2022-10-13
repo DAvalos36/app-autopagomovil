376c9772826ea2be53fa73a7ae6cdeed6fecd9af
David Avalos <davalos@itparral.edu.mx>
2022-10-12 19:50:45 -0600

    Guardado antes de refrescar lineas

--- screens/Inicio.tsx
+++ screens/Inicio.tsx
@@ -1,21 +1,37 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import {Button} from 'react-native-ui-lib'
+import {Button, GridList, Card, Image} from 'react-native-ui-lib'
 import React from 'react'
 
 import {DrawerNavigationProp} from '@react-navigation/drawer';
 import HeaderInicio from '../components/HeaderInicio';
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-type Props = {}
+type info = {
+  id: number,
+  data?: string
+}
+const ItemCard = ({id}: {id: number}) => {
+  return (
+    <Card flex centerH style={{height: 100}}>
+      <Text>Nombre Producto</Text>
+      <Card.Image style={{width: 50, height: 70}} source={{uri: 'https://images.unsplash.com/photo-1461988320302-91bde64fc8e4?ixid=2yJhcHBfaWQiOjEyMDd9&fm=jpg&q=80'}}/>
+      <Text>abc {id.toString()}</Text>
+    </Card>
+  )
+}
 
-const Inicio = ({navigation}: {navigation : DrawerNavigationProp<any>}) => {
+
+const Inicio = ({navigation}: {navigation: DrawerNavigationProp<any>}) => {
+  const renderItem = ({info}: {info: info}) => {
+    <ItemCard id={info.id} />
+  }
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <HeaderInicio navigation={navigation}/>
       <View style={{flex:10}}>
         {/* Aqui Deben De Ir Las Tarjetas De Productos Agregados */}
-        <Text>Inicio</Text>
-        <TouchableOpacity ><Text>a</Text></TouchableOpacity>
+        <GridList numColumns={2} data={[{id:1},{id:2},{id:3}]} renderItem={(e) => <ItemCard key={e.index} id={e.index} />} />
       </View>
       <View style={{flex: 2}}>
         <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', backgroundColor: 'gray'}}>
