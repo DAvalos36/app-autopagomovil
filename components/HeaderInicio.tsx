c35876e0a936399db24c1db37a0ace02ff4c57a3
David Avalos <davalos@itparral.edu.mx>
2022-12-13 15:30:34 -0600

    CAMBIO TEMPORAL PRUEBAS

--- components/HeaderInicio.tsx
+++ components/HeaderInicio.tsx
@@ -1,14 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useContext } from 'react'
 import { DrawerNavigationProp } from '@react-navigation/drawer'
 import { Ionicons, FontAwesome } from '@expo/vector-icons'
 import { COLORES } from '../Colores'
 
+import { TiendaContext } from '../context/TiendaContext' 
+
 
 const HeaderInicio = ({navigation}: {navigation : DrawerNavigationProp<any>}) => {
+  const tiendaContext = useContext(TiendaContext)
+
   return (
     <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 10}}>
-      <Ionicons name="barcode-outline" size={40} color={COLORES.logosSuperiores} />
+      <Ionicons name="barcode-outline" size={40} color={COLORES.logosSuperiores} onPress={() => void tiendaContext?.setTienda('') } />
       <FontAwesome name="user" size={40} color={COLORES.logosSuperiores} 
         onPress={() => {navigation.openDrawer()}}
         // onPress={() => {alert('hola')}}
