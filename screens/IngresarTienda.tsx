cb5d062cd94a6eb5818a11547f5ce0884c386dce
David Avalos <davalos@itparral.edu.mx>
2022-12-15 01:27:28 -0600

    feat: escaneo QR y permisos

--- screens/IngresarTienda.tsx
+++ screens/IngresarTienda.tsx
@@ -1,15 +1,35 @@
 import { StyleSheet } from 'react-native'
 import { Button, Text, Incubator, Colors, Image } from 'react-native-ui-lib'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { supabase } from '../supabase'
 import { SvgUri } from 'react-native-svg'
+import { BarCodeScanner, BarCodeScannerResult, requestPermissionsAsync, getPermissionsAsync } from 'expo-barcode-scanner';
 
 import { TiendaContext } from '../context/TiendaContext'
+import PermisoQR from './PermisoQR'
 
 
 const IngresarTienda = () => {
   const tiendaContext = useContext(TiendaContext)
+  const [escanear, setEscanear] = useState(false)
+  const [permiso, setPermiso] = useState(false)
+
+  useEffect(() => {
+    void solicitarPermiso()
+  }, [])
+
+  const solicitarPermiso = async () => {
+    const { status } = await requestPermissionsAsync()
+    if(status === 'granted'){
+      setPermiso(true)
+    }
+  }
+
+  const escaneo = ({ type, data }: BarCodeScannerResult) => {
+    setEscanear(false)
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
 
   const solicitarTienda = async (id: string) => {
     const { data, error } = await supabase
@@ -30,13 +50,23 @@ const IngresarTienda = () => {
     }
   }
 
+  if(!permiso){
+    return <PermisoQR solicitarPermiso={solicitarPermiso} />
+  }
+
+  if (escanear){
+    return <BarCodeScanner
+    onBarCodeScanned={escaneo}
+    style={StyleSheet.absoluteFillObject}
+  />
+  }
   return (
     <SafeAreaView style={styles.container}>
       <SvgUri
         uri='https://dthbdxcuhfeitijisuta.supabase.co/storage/v1/object/public/productos/EscanearTienda.svg'
         width='80%'
         height='80%'
-        onPress={() => void tiendaContext?.setTienda('idk')}
+        onPress={() => setEscanear(true)}
 
       />
 
