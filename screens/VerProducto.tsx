15ab518db5b2120485cf58fddb42264b8f0947ce
David Avalos <davalos@itparral.edu.mx>
2022-12-16 00:35:08 -0600

    wip: pantalla producto especifico

--- screens/VerProducto.tsx
+++ screens/VerProducto.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, View, ImageBackground } from 'react-native'
-import {Button, GridList, Card, Text} from 'react-native-ui-lib'
+import { Button, Image, Badge, Text } from 'react-native-ui-lib'
 import { SvgUri } from 'react-native-svg';
 import React, { useState, useEffect, useContext } from 'react'
 
@@ -20,27 +20,39 @@ type info = {
 
 const VerProducto = (nav: PantallaVerProductoProps): JSX.Element => {
   const tiendaContext = useContext(TiendaContext)
-  const {i} = nav.route.params
-
-  if (tiendaContext != null){
-    const producto = tiendaContext.productos[i]
-  }
-
+  const { i } = nav.route.params
 
   const [cantidad, setCantidad] = useState(tiendaContext?.productos[i].cantidad)
-  const [escanear, setEscanear] = useState(false)
-  const [total, setTotal] = useState(0)
+  
 
   return (
     <SafeAreaView style={styles.container}>
       {/* <ImageBackground source={require('../assets/bgInicio.png')}  style={{flex:10}}> */}
-        <SvgUri uri='https://dthbdxcuhfeitijisuta.supabase.co/storage/v1/object/public/productos/FondoPrincipal.svg'
-          height='100%' 
-          width={undefined}
-          style={StyleSheet.absoluteFillObject}
-          />
-        <Text>{tiendaContext?.productos[i].productos_info.nombre}</Text>
-        <Button label='KKAKSDKKSAKDSKAS' />
+      <SvgUri uri='https://dthbdxcuhfeitijisuta.supabase.co/storage/v1/object/public/productos/FondoPrincipal.svg'
+        height='100%'
+        width={undefined}
+        style={StyleSheet.absoluteFillObject}
+      />
+      <View>
+        <Image source={ require('../assets/LogoInicio.png') } style={{ width: 60, height: 40 }} />
+      </View>
+
+      <View style={styles.tarjeta} >
+        <Text text30>{tiendaContext?.productos[i].productos_info.nombre}</Text>
+        <Image style={styles.imgCar} source={{ uri: tiendaContext?.productos[i].productos_info.link_img }} />
+        <Text text60>${tiendaContext?.productos[i].precio.toFixed(2)}</Text>
+        <Text >{tiendaContext?.productos[i].productos_info.descripcion}</Text>
+        <View style={styles.containerBotonesTarjeta}>
+          <Button onPress={() => setCantidad(cantidad as number - 1)} label='-' outline outlineColor={COLORES.botonPrimario} outlineWidth={2} activeBackgroundColor='white' round disabled={cantidad as number <= 1} />
+          <Badge label={cantidad?.toString()} backgroundColor={COLORES.botonPrimario} size={50} />
+          <Button onPress={() => setCantidad(cantidad as number + 1)} label='+' outline outlineColor={COLORES.botonPrimario} outlineWidth={2} activeBackgroundColor='white' round />
+        </View>
+      </View>
+
+      <View>
+        <Button label='Guardar' />
+        <Button label='Eliminar' />
+      </View>
       {/* </ImageBackground> */}
     </SafeAreaView>
   )
@@ -51,13 +63,41 @@ export default VerProducto
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 10,
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
-  contenedorTexto:{
+  contenedorTexto: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'flex-end',
+  },
+  contenedorSuperior: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  tarjeta: {
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    borderColor: '#F2A007',
+    borderWidth: 1,
+    borderRadius: 10,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    padding: 10
+  },
+  containerBotonesTarjeta: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    // width: '20%',
+    marginTop: 10
+  },
+  imgCar:{
+    width: '90%',
+    aspectRatio: 1,
+    height: undefined,
+    borderRadius: 8,
+    marginVertical: 3
   }
 })
